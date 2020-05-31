608ea005d441f3f1d4b519c8fb4ccdb0d416b5d2
Parker Williamson <parker.williamson@gmail.com>
2020-05-31 21:40:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,9 +23,6 @@ class Scraper
   def self.scrape_profile_page(profile_url)
       page = Nokogiri::HTML(open(profile_url))
       student = {}
-
-      # student[:profile_quote] = page.css(".profile-quote")
-      # student[:bio] = page.css("div.description-holder p")
       container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
       container.each do |link|
         if link.include?("twitter")
